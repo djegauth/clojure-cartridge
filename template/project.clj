80483cd279e93eded7f71a5e1e2376e25533dc6a
djedev <dje.deve@gmail.com>
2014-08-09 13:22:40 +0200

    downgrade clojure -> 1.5.1

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,5 @@
 (defproject clojureapp "0.1.0-SNAPSHOT"
     :plugins [[lein-ring "0.8.11"]]
-    :dependencies [[org.clojure/clojure "1.6.0"]
+    :dependencies [[org.clojure/clojure "1.5.1"]
                    [http-kit "2.1.16"]]
     :main webapp)
